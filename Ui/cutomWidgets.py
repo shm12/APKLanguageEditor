0c7d0e3ae232f4e133ced97d49b09e26b1e97131
shm.bia <shm.bia@gmail.com>
2020-02-09 04:42:29 +0200

    Translate View ui complete

--- Ui/cutomWidgets.py
+++ Ui/cutomWidgets.py
@@ -1,5 +1,13 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                             #
+# Important!  pyqt5 versions 5.12.0 to 5.13.0 when using Python 3.7.x has a bag. Be carefull  #
+#             and don't use them.                                                             #
+#                                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 class CustomTableWidget(QtWidgets.QTableWidget):
 
     # Signals
@@ -11,10 +19,37 @@ class CustomTableWidget(QtWidgets.QTableWidget):
         self.itemClicked['QTableWidgetItem*'].connect(self.rowClickedSlot)
         self.itemChanged['QTableWidgetItem*'].connect(self.rowChangedSlot)
     
+    # Methodes
     def getRowData(self, rowNum):
         columnNum = self.columnCount()
         return [self.item(rowNum, i) for i in range(columnNum)]
+    
+    def getData(self):
+        return [self.getColumnDataInRange(i, 0, self.rowCount()) for i in range(self.columnCount())]
+    
+    def getColumnDataInRange(self, column, start, end):
+        return [self.item(i, column).text() for i in range(start, end)]
+    
+    def setColumnDataInRange(self, column, start, end, data):
+        for i in range(end - start):
+            self.item(start + i, column).setText(data[i])
 
+    def setData(self, data):
+        self.clearTableSlot()
+
+        # Preperations 
+        rows = len(data[0])
+        cellsPerRow = len(data)
+
+        # Initialize every cell
+        self.setRowCount(rows)
+        for row in range(rows):
+            for cell in range(cellsPerRow):
+                newCell = QtWidgets.QTableWidgetItem()
+                newCell.setText(str(data[cell][row]))
+                newCell.setFlags(newCell.flags() ^ QtCore.Qt.ItemIsEditable) if cell == 0 else None
+                self.setItem(row, cell, newCell)
+        
     # Slots
     @QtCore.pyqtSlot(QtWidgets.QTableWidgetItem)
     def rowClickedSlot(self, QTableWidgetItem):
@@ -31,29 +66,11 @@ class CustomTableWidget(QtWidgets.QTableWidget):
     @QtCore.pyqtSlot()
     def clearTableSlot(self):
         self.setRowCount(0)
-    
-    def setData(self, data):
-        self.clearTableSlot()
-
-        # Preperations 
-        rows = len(data[0])
-        cellsPerRow = len(data)
-
-        # Initialize every cell
-        self.setRowCount(rows)
-        print('Row Count: ', self.rowCount())
-        print('Column Count: ', self.columnCount())
-        for row in range(rows):
-            for cell in range(cellsPerRow):
-                print('Setting (', row, ', ', cell, ') = ', str(data[cell][row]))
-                newCell = QtWidgets.QTableWidgetItem()
-                newCell.setText(str(data[cell][row]))
-                self.setItem(row, cell, newCell)
-        
 class CustomTextEdit(QtWidgets.QTextEdit):
 
     # Signals
     cellTextChanged = QtCore.pyqtSignal('PyQt_PyObject','PyQt_PyObject', arguments=['tableIdx', 'Text'])
+    transalteSignal = QtCore.pyqtSignal(int, arguments=['Row'])
 
     def __init__(self, *args, **kwargs):
         super(CustomTextEdit, self).__init__(*args, **kwargs)
@@ -66,7 +83,6 @@ class CustomTextEdit(QtWidgets.QTextEdit):
     def rowChangedSlot(self, itemWidget, rowItems):
         idx = 0 if self.origin else 1
 
-        print('RowChange')
         if (itemWidget.row(), idx) == self.tableIdx:
             self.setTextByRowSlot(itemWidget, rowItems)
         
@@ -77,10 +93,15 @@ class CustomTextEdit(QtWidgets.QTextEdit):
         self.tableIdx = (itemWidget.row(), idx)
         self.setText(txt) if self.toPlainText() != txt else None
     
-
     @QtCore.pyqtSlot()
     def textChangedSlot(self):
-        self.cellTextChanged.emit(self.tableIdx, self.toPlainText())
+        if self.tableIdx:
+            self.cellTextChanged.emit(self.tableIdx, self.toPlainText())
+            
+    @QtCore.pyqtSlot()
+    def translateSlot(self):
+        if self.tableIdx:
+            self.transalteSignal.emit(self.tableIdx[0])
 
     # Properties
     @QtCore.pyqtProperty(bool)
@@ -96,14 +117,43 @@ class TranslateView(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super(TranslateView, self).__init__(*args, **kwargs)
         uic.loadUi('TranslateView.ui', self)
-        self._translationData = None
-        self.translationData = ([1,2,3,4,5,6,6], [1,2,3,4,5,6,6])
+        self.translationData = self.translationData
+        # self.translationData = ([1,2,3,4,5,6,6], [1,2,3,4,5,6,6])
     
+    def _translate(self, data):
+        """
+        Should be implemented in the logic part (this is the ui part)
+        """
+        pass
+        return [i*2 for i in data]
+    
+    # Slots
+    @QtCore.pyqtSlot(int, int)
+    def translateRangeSlot(self, start, end):
+        data = self.translationTable.getColumnDataInRange(0, start, end)
+        translatedData = self._translate(data)
+        print(start, end)
+        print(data)
+        print(translatedData)
+        self.translationTable.setColumnDataInRange(1, start, end, translatedData)
+
+    @QtCore.pyqtSlot()
+    def translateAllSlot(self):
+        print('Translate All')
+        print(self.translationTable.rowCount())
+        self.translateRangeSlot(0, self.translationTable.rowCount())
+    
+    @QtCore.pyqtSlot(int)
+    def translateRowSlot(self, row):
+        self.translateRangeSlot(row, row + 1)
+
+    # properties
     @property
     def translationData(self):
         """
         A list of two lists (identicaly in the structure).
         """
+        self._translationData = self.translationTable.getData()
         return self._translationData
     
     @translationData.setter
@@ -116,5 +166,7 @@ class TranslateView(QtWidgets.QWidget):
         assert (len(data[0]) == len(data[1])), 'TranslateView.translationData lists must be in same lenght'
 
         self.translationTable.setData(data)
+        self.translationTextEdit.tableIdx = None
+        self.originTextEdit.tableIdx = None
 
 
