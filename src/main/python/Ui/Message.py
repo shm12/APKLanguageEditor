327ce0c45fcdc7ada379e80cf76e5270fa6a2fbe
shm.bia <shm.bia@gmail.com>
2020-03-09 00:35:52 +0200

    improve message class

--- src/main/python/Ui/Message.py
+++ src/main/python/Ui/Message.py
@@ -7,17 +7,46 @@ from appctx import ApplicationContext
 UI_FILE = ApplicationContext.get_resource(os.path.join('ui', 'Message.ui'))
 
 class Message(getUiClass(UI_FILE)):
+    DEFAUL_TITLE = 'Attention'
+    DEFAUL_MESAGE = 'A message'
+    DEFAUL_ICON = ApplicationContext.get_resource(os.path.join('icons', 'attention.png'))
 
-    def __init__(self, *args, message='An error occure', **kwargs):
+    def __init__(self, *args,
+                 title=None,
+                 message=None,
+                 icon=None,
+                 **kwargs):
         super(Message, self).__init__(*args, **kwargs)
-        self.message = message
-    
+        # Initialize properties
+        self._icon_path = None
+        self.ret = None
+        self.title = title if title else self.DEFAUL_TITLE
+        self.message = message if message else self.DEFAUL_MESAGE
+        self.icon = icon if icon else self.DEFAUL_ICON
+
     @property
     def message(self):
-        return self._message
-    
+        return self.messageLable.text()
+
     @message.setter
     def message(self, message):
-        self._message = message
-        self.label.setText(self._message)
+        self.messageLable.setText(message)
         self.adjustSize()
+
+    @property
+    def title(self):
+        return self.windowTitle()
+
+    @title.setter
+    def title(self, title):
+        self.setWindowTitle(title)
+
+    @property
+    def icon(self):
+        return self._icon_path
+
+    @icon.setter
+    def icon(self, path):
+        self._icon_path = path
+        self.iconLable.setPixmap(QtGui.QPixmap(path))
+
